70ba5d86dcc33a793681060f42e998d7b2392df5
Franz Schnyder <schnyderfranz@gmail.com>
2014-02-02 13:43:10 +0100

    Try to fix CI build problem

--- BtBuildFramework/source/platform/osx.platform.rb
+++ BtBuildFramework/source/platform/osx.platform.rb
@@ -11,7 +11,7 @@ class OsXBuilder < Builder
     @ar = "ar";
     
     @cxxflags = "-Wall -Werror=return-type -Wno-unused-local-typedefs -std=c++11 -g -DBT_PF_OSX -D_GLIBCXX_USE_NANOSLEEP=1"
-    @linkflags = ""
+    @linkflags = "-pthread"
           
     @includes = ""
     @libraries = ""
