3567f7dade78956812183781567e732163915598
Franz Schnyder <schnyderfranz@gmail.com>
2014-04-06 17:51:18 +0200

    Add linux as host debug option

--- BtBuildFramework/source/configuration/host_debug.config.rb
+++ BtBuildFramework/source/configuration/host_debug.config.rb
@@ -5,9 +5,27 @@ class Configuration
   protected
   
   def setup()
+       
+    if is_osx?
+      self[:platform] = "osx";
+    elsif is_linux?
+      self[:platform] = "linux";
+    else
+      throw "unsupported host"
+    end   
+      
     self[:is_host] = true;
-    self[:platform] = "osx";
     self[:test_filter] = "-*IntegrationTest.*";
+      
+  end
+  
+  
+  def is_osx?
+    RUBY_PLATFORM.downcase.include?("darwin")
+  end
+   
+  def is_linux?
+    RUBY_PLATFORM.downcase.include?("linux")
   end
   
 end
---
+++ BtBuildFramework/source/platform/linux.platform.rb
@@ -0,0 +1,36 @@
+require 'builder'
+require 'platform/tool/tools'
+require 'platform/testing/runners'
+
+class LinuxBuilder < Builder
+  
+  def setUp()
+    @platform = "linux"
+    
+    @cc = "gcc";
+    @cxx = "g++";
+    @ar = "ar";
+    
+    @ccflags = "-Wall -Werror=return-type -g -DBT_PF_OSX -D_GLIBCXX_USE_NANOSLEEP=1 -DUSE_NAMED_SEMAPHORES"
+    @cxxflags = "-Wall -Werror=return-type -std=c++11 -g -DBT_PF_OSX -D_GLIBCXX_USE_NANOSLEEP=1"
+    @linkflags = "--pthread"
+          
+    @includes = ""
+    @libraries = ""
+      
+    @tool = Tools::GCC
+    @runner = HostRunner.new  
+      
+  end  
+end
+
+
+class Platform
+
+  protected
+  
+  def setup
+    self[:builder ] = LinuxBuilder.new(@configuration, File.expand_path(File.join(@directory,"gcc")))
+  end  
+  
+end
