e2a37dbc093c58eaefb9199ebdc594b6e0e6052f
Franz Schnyder <schnyderfranz@gmail.com>
2015-11-29 16:04:11 +0100

    Vargrant configuration for linux box to cross compile.

--- BtBuildFramework/source/platform/pi.platform.rb
+++ BtBuildFramework/source/platform/pi.platform.rb
@@ -7,20 +7,44 @@ class PiBuilder < Builder
   def setUp()
     @platform = "pi"
     
-    @cc = "/Volumes/Crosstool/arm-pi-linux-gnueabi/bin/arm-pi-linux-gnueabi-gcc";        
-    @cxx = "/Volumes/Crosstool/arm-pi-linux-gnueabi/bin/arm-pi-linux-gnueabi-g++";
-    @ar = "/Volumes/Crosstool/arm-pi-linux-gnueabi/bin/arm-pi-linux-gnueabi-ar";
     
-    # @cc =  "/Volumes/xtools/arm-none-linux-gnueabi/bin/arm-none-linux-gnueabi-gcc";        
-    # @cxx = "/Volumes/xtools/arm-none-linux-gnueabi/bin/arm-none-linux-gnueabi-g++";
-    # @ar =  "/Volumes/xtools/arm-none-linux-gnueabi/bin/arm-none-linux-gnueabi-ar";
+    if RUBY_PLATFORM.downcase.include?("darwin")   
+      @cc = "/Volumes/Crosstool/arm-pi-linux-gnueabi/bin/arm-pi-linux-gnueabi-gcc";        
+      @cxx = "/Volumes/Crosstool/arm-pi-linux-gnueabi/bin/arm-pi-linux-gnueabi-g++";
+      @ar = "/Volumes/Crosstool/arm-pi-linux-gnueabi/bin/arm-pi-linux-gnueabi-ar";
+      
+      # @cc =  "/Volumes/xtools/arm-none-linux-gnueabi/bin/arm-none-linux-gnueabi-gcc";        
+      # @cxx = "/Volumes/xtools/arm-none-linux-gnueabi/bin/arm-none-linux-gnueabi-g++";
+      # @ar =  "/Volumes/xtools/arm-none-linux-gnueabi/bin/arm-none-linux-gnueabi-ar";
+      
+      @ccflags = "-Wall -Werror=return-type -Wno-unused-local-typedefs -g -DBT_PF_PI -D_GLIBCXX_USE_NANOSLEEP=1 -D_GLIBCXX_USE_SCHED_YIELD=1"
+      @cxxflags = "-Wall -Werror=return-type -std=c++11 -D_GLIBCXX_USE_NANOSLEEP=1 -D_GLIBCXX_USE_SCHED_YIELD=1 -g -DBT_PF_PI -pthread"
+      @linkflags = "-pthread -lrt -L/Volumes/Crosstool/usr/local/lib"
+      
+      @includes = "-I/Volumes/Crosstool/usr/local/include"
+      @libraries = ""
+    else 
+      @cc = "arm-linux-gnueabihf-gcc";        
+      @cxx = "arm-linux-gnueabihf-g++";
+      @ar = "arm-linux-gnueabihf-ar";
+      
+      # @cc =  "/Volumes/xtools/arm-none-linux-gnueabi/bin/arm-none-linux-gnueabi-gcc";        
+      # @cxx = "/Volumes/xtools/arm-none-linux-gnueabi/bin/arm-none-linux-gnueabi-g++";
+      # @ar =  "/Volumes/xtools/arm-none-linux-gnueabi/bin/arm-none-linux-gnueabi-ar";
+      
+      @ccflags = "-Wall -Werror=return-type -Wno-unused-local-typedefs -g -DBT_PF_PI -D_GLIBCXX_USE_NANOSLEEP=1 -D_GLIBCXX_USE_SCHED_YIELD=1"
+      @cxxflags = "-Wall -Werror=return-type -std=c++11 -D_GLIBCXX_USE_NANOSLEEP=1 -D_GLIBCXX_USE_SCHED_YIELD=1 -g -DBT_PF_PI -pthread"
+      @linkflags = "-pthread -lrt -L/home/vagrant/rpi/tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64/lib"
+      
+      @includes = "-I/home/vagrant/rpi/boost"
+      @libraries = ""
+      
+      
+      
+        
+    end
     
-    @ccflags = "-Wall -Werror=return-type -Wno-unused-local-typedefs -g -DBT_PF_PI -D_GLIBCXX_USE_NANOSLEEP=1 -D_GLIBCXX_USE_SCHED_YIELD=1"
-    @cxxflags = "-Wall -Werror=return-type -std=c++11 -D_GLIBCXX_USE_NANOSLEEP=1 -D_GLIBCXX_USE_SCHED_YIELD=1 -g -DBT_PF_PI -pthread"
-    @linkflags = "-pthread -lrt -L/Volumes/Crosstool/usr/local/lib"
     
-    @includes = "-I/Volumes/Crosstool/usr/local/include"
-    @libraries = ""
       
     @tool = Tools::GCC 
     @runner = SSHRunner.new  
